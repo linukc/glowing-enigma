a27213dc236726be3db2f2d196dbdee5037ed5b3
Sergei Linok <serega.linok5555@yandex.ru>
2021-05-09 23:26:37 +0300

    added simple bot's behavior

--- heroes.py
+++ heroes.py
@@ -18,6 +18,7 @@ class Bomb(pygame.sprite.Sprite):
         self.opponent = opponent
         self.is_exploded = False
         self.init_time = pygame.time.get_ticks()
+        self.radius = 200 # for potensial collide with bot check
 
     def update(self, *args):
         self.rect = self.rect.move(random.randrange(3) - 1, 
@@ -53,6 +54,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.x = pos[0]
         self.rect.y = pos[1]
         self.vx = 20
+        self.radius = 500 # for potensial collide with bot check
 
     def update(self, *args):
         if self.orientation == "leftward":
@@ -124,18 +126,13 @@ class Base_Hero(pygame.sprite.Sprite):
         if self.mode == "player":
             self.xp = 50
             self.max_xp = 50
-            self.y_step = 7
+            self.y_step = 8
 
-    def update(self, *args):
-
-        keys = args[0]
-        #XP bar
-        pygame.draw.rect(self.image, WHITE, (0, 0, self.rect.width, 10), border_radius=2)
-        pygame.draw.rect(self.image, RED, (0, 0, self.xp*(self.rect.width/self.max_xp), 10), border_radius=2)
-        pygame.draw.rect(self.image, BLACK, (0, 0, self.rect.width, 10), width=1, border_radius=2)
-
-        #base cast (shot)
-        if keys[self.first_cast] and not self.locked:
+    def make_first_cast(self, keys=None):
+        button_is_pressed = True
+        if self.mode == "player":
+            button_is_pressed = keys[self.first_cast]
+        if button_is_pressed and not self.locked:
             time = pygame.time.get_ticks()
             if time - self.first_cast_pressed > 1000:
                 if self.orientation == "leftward":
@@ -146,8 +143,11 @@ class Base_Hero(pygame.sprite.Sprite):
                         pygame.sprite.GroupSingle(self.get_opponent()))
                 self.first_cast_pressed = time
 
-        #second cast (random bombs)
-        if keys[self.second_cast] and not self.locked:
+    def make_second_cast(self, keys=None):
+        button_is_pressed = True
+        if self.mode == "player":
+            button_is_pressed = keys[self.second_cast]
+        if button_is_pressed and not self.locked:
             time = pygame.time.get_ticks()
             if time - self.second_cast_pressed > 7000:
                 if self.orientation == "leftward":
@@ -159,6 +159,18 @@ class Base_Hero(pygame.sprite.Sprite):
                         Bomb(range(6, int(0.9*WIN_WIDTH//2)), range(6, int(0.9*WIN_HEIGHT)),
                                         pygame.sprite.GroupSingle(self.get_opponent()))
                 self.second_cast_pressed = time
+    
+    def update(self, *args):
+
+        keys = args[0]
+        #XP bar
+        pygame.draw.rect(self.image, WHITE, (0, 0, self.rect.width, 10), border_radius=2)
+        pygame.draw.rect(self.image, RED, (0, 0, self.xp*(self.rect.width/self.max_xp), 10), border_radius=2)
+        pygame.draw.rect(self.image, BLACK, (0, 0, self.rect.width, 10), width=1, border_radius=2)
+
+        if self.mode == "player":
+            self.make_first_cast(keys)
+            self.make_second_cast(keys)
 
         x = 0
         y = 0
@@ -166,12 +178,50 @@ class Base_Hero(pygame.sprite.Sprite):
         if self.mode == "player" and not self.locked:
             x = self.x_step * (keys[self.right] - keys[self.left])
             y = self.y_step* (keys[self.down] - keys[self.up])
-        elif not self.locked:#auto
-            x = 0
+            self.rect = self.rect.move((x, y))
+        elif not self.locked:
+            #auto
+            if random.choice([0, 1, 1]):
+                if self.orientation == "rightward":
+                    x = -int(self.x_step // 2)
+                else:
+                    x = int(self.x_step // 2)
             delta_y = self.rect.y - self.get_opponent().rect.y
             y = -self.y_step if delta_y>self.y_step else (self.y_step if delta_y<-self.y_step else 0) #if elif else
+            
+            bullets = bullets_sprites.sprites()
+            for bullet in bullets:
+                if pygame.sprite.collide_circle(self, bullet):
+                    if random.choice([0, 1, 1]):
+                        if self.orientation == "rightward":
+                            y += self.y_step
+                            x += self.x_step
+                        else:
+                            y -= self.y_step
+                            x -= self.x_step
+            
+            bombs = bombs_sprites.sprites()
+            for bomb in bombs:
+                if pygame.sprite.collide_circle(self, bomb):
+                    if random.choice([0, 1, 1]):
+                        if self.orientation == "rightward":
+                            x += self.x_step
+                        else:
+                            x -= self.x_step
+                    if random.choice([0, 1, 1]):
+                        if self.orientation == "rightward":
+                            y += -self.y_step
+                        else:
+                            y -= -self.y_step
 
-        self.rect = self.rect.move((x, y))
+            x = self.x_step if x > 0 else(-self.x_step if x < 0 else 0)
+            y = self.y_step if y > 0 else(-self.y_step if y < 0 else 0)
+            self.rect = self.rect.move((x, y))
+
+            #bot's actions
+            if abs(self.rect.center[1] - self.get_opponent().rect.center[1]) <= 450:
+                self.make_first_cast()
+            self.make_second_cast()
 
         #collidence with borders
         border_offset = 5
